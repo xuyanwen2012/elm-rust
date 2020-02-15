d9c792f3b6b2f53ce0f54450a2e05d453b86a6e3
Ivan Xu <info@chivalrystudio.com>
2020-02-14 18:19:10 -0800

    setup lalrpop

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,8 +1,25 @@
 pub mod token;
 
+#[macro_use]
+extern crate lalrpop_util;
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod!(
+    pub calculator1
+);
+
 #[cfg(test)]
 mod tests {
     use crate::token::Tok;
+
+    #[test]
+    fn test_lalrpop_calculator() {
+        use super::calculator1;
+        assert!(calculator1::TermParser::new().parse("22").is_ok());
+        assert!(calculator1::TermParser::new().parse("(22)").is_ok());
+        assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
+        assert!(calculator1::TermParser::new().parse("((22)").is_err());
+    }
 
     #[test]
     fn it_works() {
