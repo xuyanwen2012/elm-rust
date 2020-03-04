3a7c77df7c02c80f187b9e84547cb1e2b74f9dc9
Ivan Xu <info@chivalrystudio.com>
2020-03-03 16:37:52 -0800

    [type] basic type checker, incomplete

--- analyzer/src/typechecker.rs
+++ analyzer/src/typechecker.rs
@@ -1,29 +1,94 @@
 use std::collections::HashMap;
-use std::sync::Mutex;
 
 use rustelm_parser::ast;
+use rustelm_parser::ast::{Atom, Expr};
 
-lazy_static! {
-    static ref HASHMAP: Mutex<HashMap<u32, &'static str>> = {
-        let mut m = HashMap::new();
-        m.insert(0, "foo");
-        m.insert(1, "bar");
-        m.insert(2, "baz");
-        Mutex::new(m)
-    };
+type Context = HashMap<u32, String>;
+
+#[derive(PartialEq)]
+pub enum Types {
+    Unit,
+    Int,
+    Abs(Box<Types>, Box<Types>),
 }
 
-pub fn typecheck_root(root: ast::Expr) {
-    let context_table = HashMap::<u32, String>::new();
+pub fn typecheck_root(root: Box<ast::Expr>) -> Types {
+    let env = Context::new();
+
+    typecheck(&env, root)
+}
+
+fn get_type_from_ctx(env: &Context, name: String) -> Types {
+    Types::Unit
+}
+
+fn typecheck(env: &Context, term: Box<ast::Expr>) -> Types {
+    use Types::*;
+    match *term {
+        Expr::Const(atom) => match atom {
+            Atom::Unit => Unit,
+            Atom::Num(_) => Int,
+            Atom::Var(var) => get_type_from_ctx(env, var),
+        },
+        Expr::Abs(args, expr) => {
+            // Add bindings to context
+            // TODO: Fix this
+
+            for arg in args {}
+
+            Unit
+        }
+        Expr::App(e1, e2) => {
+            let type_e1 = typecheck(env, e1);
+            let type_e2 = typecheck(env, e2);
+
+            match type_e1 {
+                Abs(t11, t12) => {
+                    if type_e2 == *t11 {
+                        *t12
+                    } else {
+                        // TODO: Error
+                        Unit
+                    }
+                }
+                // TODO: Error
+                _ => Unit,
+            }
+        }
+        Expr::BinOp(l, _, r) => {
+            if Int == typecheck(env, r) && Int == typecheck(env, l) {
+                Int
+            } else {
+                // TODO: Error
+                Unit
+            }
+        }
+        Expr::If(pred, e1, e2) => {
+            if Int == typecheck(env, pred) {
+                let type_e1 = typecheck(env, e1);
+                if type_e1 == typecheck(env, e2) {
+                    type_e1
+                } else {
+                    // TODO: Error
+                    Unit
+                }
+            } else {
+                // TODO: Error
+                Unit
+            }
+        }
+        Expr::Let(bindings, expr) => {
+            // Add bindings
+
+            Unit
+        }
+        Expr::Signal(_) => Unit,
+        Expr::Lift(_, _) => Unit,
+        Expr::Foldp(_, _, _) => Unit,
+    }
 }
 
 mod test {
-    use super::HASHMAP;
-
     #[test]
-    fn test_hashmap() {
-        assert_eq!(HASHMAP.lock().unwrap().get(&1).unwrap(), &"bar");
-        HASHMAP.lock().unwrap().insert(3, "fff");
-        assert_eq!(HASHMAP.lock().unwrap().get(&3).unwrap(), &"fff");
-    }
+    fn test_hashmap() {}
 }
